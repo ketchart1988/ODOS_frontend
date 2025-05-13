cc49b12c1eb59d35a82d6aac2bc5f468240c4e98
ketchart1988 <ketchart.kaewplee@gmail.com>
2025-05-13 17:10:01 +0700

    Initial commit for 13-05-25(2)

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -10,6 +10,8 @@ const ResultTable = ({ result }) => (
             <th className="px-2 sm:px-6 py-2 sm:py-4 text-left font-medium border-b border-gray-300">ชื่อ</th>
             <th className="px-2 sm:px-6 py-2 sm:py-4 text-left font-medium border-b border-gray-300">นามสกุล</th>
             <th className="px-2 sm:px-6 py-2 sm:py-4 text-left font-medium border-b border-gray-300">สถานะ</th>
+            <th className="px-2 sm:px-6 py-2 sm:py-4 text-left font-medium border-b border-gray-300">คลิบวีดีโอ</th>
+            <th className="px-2 sm:px-6 py-2 sm:py-4 text-left font-medium border-b border-gray-300">เอกสาร</th>
           </tr>
         </thead>
         <tbody>
@@ -18,6 +20,43 @@ const ResultTable = ({ result }) => (
             <td className="border px-2 sm:px-6 py-2 sm:py-3">{result["ชื่อ (ภาษาไทย)"] || "ไม่ระบุ"}</td>
             <td className="border px-2 sm:px-6 py-2 sm:py-3">{result["นามสกุล (ภาษาไทย)"] || "ไม่ระบุ"}</td>
             <td className="border px-2 sm:px-6 py-2 sm:py-3">{result.Status || "ไม่ระบุ"}</td>
+            <td className="border px-2 sm:px-6 py-2 sm:py-3">
+  {result["แนบ Link Youtube (เปิดแบบ unlisted)"] ? (
+    <a
+      href={
+        result["แนบ Link Youtube (เปิดแบบ unlisted)"].startsWith("http")
+          ? result["แนบ Link Youtube (เปิดแบบ unlisted)"]
+          : `https://${result["แนบ Link Youtube (เปิดแบบ unlisted)"]}`
+      }
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-blue-600 underline"
+    >
+      VDO Link
+    </a>
+  ) : (
+    "ไม่ระบุ"
+  )}
+</td>
+<td className="border px-2 sm:px-6 py-2 sm:py-3">
+  {result["เอกสารสำคัญ Google Drive Link (shared)"] ? (
+    <a
+      href={
+        result["เอกสารสำคัญ Google Drive Link (shared)"].startsWith("http")
+          ? result["เอกสารสำคัญ Google Drive Link (shared)"]
+          : `https://${result["เอกสารสำคัญ Google Drive Link (shared)"]}`
+      }
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-blue-600 underline"
+    >
+      Doc Link
+    </a>
+  ) : (
+    "ไม่ระบุ"
+  )}
+</td>
+            {/* <td className="border px-2 sm:px-6 py-2 sm:py-3">{result["เอกสารสำคัญ Google Drive Link (shared)"] || "ไม่ระบุ"}</td> */}
           </tr>
         </tbody>
       </table>
