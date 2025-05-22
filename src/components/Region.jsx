d1e22cdd9c606bcd52746434934298ddc1d2366f
LittleW <w-army@hotmail.co.th>
2025-05-22 11:14:51 +0700

    added carousel to region part

--- src/components/Region.jsx
+++ src/components/Region.jsx
@@ -1,229 +1,118 @@
 // import React from "react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ImageModal from "../components/RegionPop";
+import RegionButton from "./RegionButton";
 
 function Region() {
+  // default: False
   const [showModal, setShowModal] = useState(false);
-  const [modalImage, setModalImage] = useState("");
-  const [modalAlt, setModalAlt] = useState("");
+  const [modalImageSrc, setModalImageSrc] = useState(); 
+  // array of image and alt 
+  // 
+  //     {
+  //         src: ["image1", "image2"], 
+  //         alt: "UK"
+  //     }
+  // 
 
-  const openModal = (image, alt) => {
-    setModalImage(image);
-    setModalAlt(alt);
+  // handle Modal
+  const openModal = (images, alts) => {
+    setModalImageSrc({src: images, alt: alts});
     setShowModal(true);
   };
+
+  useEffect(() => {
+    console.log(modalImageSrc);
+  }, [modalImageSrc])
+
+  // ['']
+
   return (
     <div className="w-full h-auto p-5 bg-[url('/images/ODOS%20Website_EN_BG%20Section%202.png')] bg-cover bg-center" >
-      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 bg-[url('/images/ODOS Website_EN_BG Section 2.png')] bg-cover bg-center "> */}
-      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 bg-[url('/images/ODOS Website_EN_BG Section 2.png')] bg-cover bg-center"> */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 ">
+        <div className="text-center" >
+          <p className="text-2xl sm:text-3xl font-bold ">North America</p>
+          <RegionButton
+            handleModal={openModal}
+            countryCode={"USA"}
+            imageArray={["/images/04_USA.JPG", "/images/04_USA.JPG"]}
+          />
+        </div>
 
         <div className="text-center" >
-          <p className="text-2xl sm:text-3xl font-bold ">NORTH AMERICA</p>
-          <div className="text-lg sm:text-xl mt-2">
-            <p>
-
-              {/* <a
-                // href="/pdf/japan.pdf"                // target="_blank"
-                rel="noopener noreferrer"
-                 className="hover:text-[#869bd4]"
-              >
-                USA
-              </a> */}
-
-            <a
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
-                openModal("/images/04_USA.JPG", "USA");
-                // src="/images/ODOS Website_EN_Section 5 Declaration.jpg"
-              }}
-              rel="noopener noreferrer"
-              className="hover:text-[#869bd4]"
-            >
-              USA
-            </a>
-
-            </p>
-          </div>
-        </div>
-
-        <div className="text-center">
-          <p className="text-2xl sm:text-3xl font-bold mt-4 sm:mt-0">EUROPE</p>
-          <div className="text-lg sm:text-xl flex flex-col mt-2 ">
-            <a
-              //href="/pdf/japan.pdf"              // target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-[#869bd4]"
-            >
-              UK
-            </a>
-            {/* <a
-              //href="/pdf/japan.pdf"              // target="_blank"
-              rel="noopener noreferrer"
-               className="hover:text-[#869bd4]"
-            >
-              Germany
-            </a> */}
-            <a
-              //href="/pdf/japan.pdf"              // target="_blank"
-              rel="noopener noreferrer"
-               className="hover:text-[#869bd4]"
-            >
-              Sweden
-            </a>
-            <a
-              //href="/pdf/japan.pdf"              // target="_blank"
-              rel="noopener noreferrer"
-               className="hover:text-[#869bd4]"
-            >
-              Finland
-            </a>
-
-            {/* <a
-              //href="/pdf/japan.pdf"              // target="_blank"
-              rel="noopener noreferrer"
-               className="hover:text-[#869bd4]"
-            >
-              Estonia
-            </a> */}
-
-            <a
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
-                openModal("/images/03_Estonia.JPG", "Estonia");
-                // src="/images/ODOS Website_EN_Section 5 Declaration.jpg"
-              }}
-              rel="noopener noreferrer"
-              className="hover:text-[#869bd4]"
-            >
-              Estonia
-            </a>
-
-          </div>
-        </div>
-
-        <div className="text-center">
-          <p className="text-2xl sm:text-3xl font-bold mt-4 sm:mt-0">
-            AUSTRALIA
-          </p>
-          <div className="text-lg sm:text-xl flex flex-col mt-2">
-            {/* <a
-              //href="/pdf/japan.pdf"              // target="_blank"
-              rel="noopener noreferrer"
-               className="hover:text-[#869bd4]"
-            >
-              Australia
-            </a> */}
-
-<a
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
-                openModal("/images/01_AUS.JPG", "Australia");
-                // src="/images/ODOS Website_EN_Section 5 Declaration.jpg"
-              }}
-              rel="noopener noreferrer"
-              className="hover:text-[#869bd4]"
-            >
-              Australia
-            </a>
-
-            {/* <a
-              //href="/pdf/japan.pdf"              // target="_blank"
-              rel="noopener noreferrer"
-               className="hover:text-[#869bd4]"
-            >
-              New zealand
-            </a> */}
-          </div>
-        </div>
-
-        <div className="text-center">
-          <p className="text-2xl sm:text-3xl font-bold mt-4 sm:mt-0">ASIA</p>
-          <div className="text-lg sm:text-xl flex flex-col mt-2 ">
+          <p className="text-2xl sm:text-3xl font-bold ">Europe</p>
+              <RegionButton
+                handleModal={openModal}
+                countryCode={"UK"}
+                imageArray={["/images/uk_placeholder.JPG"]}
+              />
+          
+              <RegionButton
+                handleModal={openModal}
+                countryCode={"Sweden"}
+                imageArray={["/images/sweden_placeholder.JPG"]}
+              />
             
-            {/* <a 
-              //href="/pdf/japan.pdf"              // target="_blank"
-              rel="noopener noreferrer"
-               className="hover:text-[#869bd4]"
-            >
-              China
-            </a> */}
-
-            <a
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
-                openModal("/images/05_China.JPG", "China");
-                // src="/images/ODOS Website_EN_Section 5 Declaration.jpg"
-              }}
-              rel="noopener noreferrer"
-              className="hover:text-[#869bd4]"
-            >
-              China
-            </a>
-
-            {/* <a
-              
-              rel="noopener noreferrer"
-               className="hover:text-[#869bd4]"
-            >
-              India
-            </a> */}
-
+              <RegionButton
+                handleModal={openModal}
+                countryCode={"Finland"}
+                imageArray={["/images/finland_placeholder.JPG"]}
+              />
             
-            {/* <a
+              <RegionButton
+                handleModal={openModal}
+                countryCode={"Estonia"}
+                imageArray={["/images/03_Estonia.JPG"]}
+              />
+        </div>
 
-              rel="noopener noreferrer"
-               className="hover:text-[#869bd4]"
-            >
-              Japan
-            </a> */}
+        <div className="text-center" >
+          <p className="text-2xl sm:text-3xl font-bold ">Australia</p>
+          <RegionButton
+            handleModal={openModal}
+            countryCode={"Australia"}
+            imageArray={["/images/01_AUS.JPG"]}
+          />
+        </div>
 
-<a
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
-                openModal("/images/02_JAP.JPG", "Japan");
-                // src="/images/ODOS Website_EN_Section 5 Declaration.jpg"
-              }}
-              rel="noopener noreferrer"
-              className="hover:text-[#869bd4]"
-            >
-              Japan
-            </a>
-
-            <a
-              rel="noopener noreferrer"
-               className="hover:text-[#869bd4]"
-            >
-              Korea
-            </a>
-            <a
-              //href="/pdf/japan.pdf"              // target="_blank"
-              rel="noopener noreferrer"
-               className="hover:text-[#869bd4]"
-            >
-              Singapore
-            </a>
-            <a
-              //href="/pdf/japan.pdf"              // target="_blank"
-              rel="noopener noreferrer"
-               className="hover:text-[#869bd4]"
-            >
-              Taiwan
-            </a>
-          </div>
+        <div className="text-center" >
+          <p className="text-2xl sm:text-3xl font-bold ">Asia</p>
+          <RegionButton
+            handleModal={openModal}
+            countryCode={"China"}
+            imageArray={["/images/05_China.JPG"]}
+          />
+        
+          <RegionButton
+            handleModal={openModal}
+            countryCode={"Japan"}
+            imageArray={["/images/02_JAP.JPG", "/images/03_Estonia.JPG"]}
+          />
+        
+          <RegionButton
+            handleModal={openModal}
+            countryCode={"Korea"}
+            imageArray={["/images/korea_placeholder.JPG"]}
+          />
+        
+          <RegionButton
+            handleModal={openModal}
+            countryCode={"Singapore"}
+            imageArray={["/images/singapore_placeholder.JPG"]}
+          />
+        
+          <RegionButton
+            handleModal={openModal}
+            countryCode={"Taiwan"}
+            imageArray={["/images/taiwan_placeholder.JPG"]}
+          />
         </div>
       </div>
 
       {/* Modal */}
       {showModal && (
         <ImageModal
-          imageSrc={modalImage}
-          altText={modalAlt}
+          images={modalImageSrc}
           onClose={() => setShowModal(false)}
         />
       )}
--- src/components/RegionPop.jsx
+++ src/components/RegionPop.jsx
@@ -1,25 +1,58 @@
 // components/ImageModal.js
-import React from "react";
 
-const ImageModal = ({ imageSrc, altText, onClose }) => {
-  console.log("กำลังแสดงรูป:", imageSrc); // ✅ ใช้ console.log แทน print
+import { useRef, useState } from "react";
+import CarouselButton from "./CarouselButton";
 
-    return (
+const ImageModal = ({ images, onClose }) => {
 
+  const sliderPositionRef = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(0); // image index translate-x-[ref%]
+
+  const handleLeft = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(prev => prev - 1);
+    }
+  }
+
+  const handleRight = () => {
+    if (currentIndex != images.src.length - 1) {
+      setCurrentIndex(prev => prev + 1);
+    }
+  }
+
+  return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white rounded-xl p-4 shadow-xl relative max-w-3xl w-full">
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 text-gray-600 hover:text-red-600 text-xl"
+      <div className="bg-white border-white border-[1rem] rounded-xl relative max-w-3xl w-full overflow-hidden">
+        
+        <div
+          className="flex w-full transition-transform duration-300 duration-700"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }} 
         >
-          ✕
-        </button>
-        <img
-          src={imageSrc}
-        //   src="/images/02_JAP.JPG"
-          alt={altText}
-          className="w-full h-auto rounded"
-        />
+          {
+            images.src.map((img, i) => 
+              <img
+                key={i}
+                src={img}
+                alt={images.alt}
+                className="w-full h-auto rounded"
+              />
+            )
+          }
+        </div>
+        <div className="bg text-xl text-gray-600">
+          <button
+            onClick={onClose}
+            className="absolute top-2 right-2 hover:text-red-600 text-xl"
+          >
+            ✕
+          </button>
+          { images.src.length > 1 &&
+            <>
+              <CarouselButton handleButton={handleLeft} direction={'<'} className="text-white text-5xl absolute top-1/2 translate-y-[-50%] left-5 hover:scale-[200%] transition-all duration-500 active:scale-150" />
+              <CarouselButton handleButton={handleRight} direction={'>'} className="text-white text-5xl absolute top-1/2 translate-y-[-50%] right-5 hover:scale-[200%] transition-all duration-500 active:scale-150" />
+            </>
+          }
+        </div>
       </div>
     </div>
   );
