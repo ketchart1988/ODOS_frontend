ee1ee2fa923b4184cd6c7dbdc75ccb516bff0b2c
KrittapasP <krittapas.kaha@gmail.com>
2025-05-28 10:38:20 +0700

    fixed images on sliders

---
+++ src/components/ResultTable.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+const ResultTable = ({ result }) => (
+  <div className="mt-24 overflow-visible rounded-xl shadow-lg">
+    <div className="w-full">
+      <table className="table-auto w-full mt-6 bg-[#0B293B] text-xs sm:text-sm border rounded-xl font-sukhumvit">
+        <thead>
+          <tr className="text-white">
+            <th className="px-2 sm:px-6 py-2 sm:py-4 text-left font-medium border-b border-gray-300">รหัสผู้สมัคร</th>
+            <th className="px-2 sm:px-6 py-2 sm:py-4 text-left font-medium border-b border-gray-300">ชื่อ</th>
+            <th className="px-2 sm:px-6 py-2 sm:py-4 text-left font-medium border-b border-gray-300">นามสกุล</th>
+            <th className="px-2 sm:px-6 py-2 sm:py-4 text-left font-medium border-b border-gray-300">สถานะ</th>
+            <th className="px-2 sm:px-6 py-2 sm:py-4 text-left font-medium border-b border-gray-300">คลิบวีดีโอ</th>
+            <th className="px-2 sm:px-6 py-2 sm:py-4 text-left font-medium border-b border-gray-300">เอกสาร</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="even:bg-gray-50 odd:bg-white text-gray-800">
+            <td className="border px-2 sm:px-6 py-2 sm:py-3">{result.Application_id || "ไม่ระบุ"}</td>
+            <td className="border px-2 sm:px-6 py-2 sm:py-3">{result["ชื่อ (ภาษาไทย)"] || "ไม่ระบุ"}</td>
+            <td className="border px-2 sm:px-6 py-2 sm:py-3">{result["นามสกุล (ภาษาไทย)"] || "ไม่ระบุ"}</td>
+            <td className="border px-2 sm:px-6 py-2 sm:py-3">{result.Status || "ไม่ระบุ"}</td>
+            <td className="border px-2 sm:px-6 py-2 sm:py-3">
+  {result["แนบ Link Youtube (เปิดแบบ unlisted)"] ? (
+    <a
+      href={
+        result["แนบ Link Youtube (เปิดแบบ unlisted)"].startsWith("http")
+          ? result["แนบ Link Youtube (เปิดแบบ unlisted)"]
+          : `https://${result["แนบ Link Youtube (เปิดแบบ unlisted)"]}`
+      }
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-blue-600 underline"
+    >
+      VDO Link
+    </a>
+  ) : (
+    "ไม่ระบุ"
+  )}
+</td>
+<td className="border px-2 sm:px-6 py-2 sm:py-3">
+  {result["เอกสารสำคัญ Google Drive Link (shared)"] ? (
+    <a
+      href={
+        result["เอกสารสำคัญ Google Drive Link (shared)"].startsWith("http")
+          ? result["เอกสารสำคัญ Google Drive Link (shared)"]
+          : `https://${result["เอกสารสำคัญ Google Drive Link (shared)"]}`
+      }
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-blue-600 underline"
+    >
+      Doc Link
+    </a>
+  ) : (
+    "ไม่ระบุ"
+  )}
+</td>
+            {/* <td className="border px-2 sm:px-6 py-2 sm:py-3">{result["เอกสารสำคัญ Google Drive Link (shared)"] || "ไม่ระบุ"}</td> */}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  
+  
+);
+
+export default ResultTable;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,26 @@
+import ScrollToTop from "../components/ScrollToTop";
+import Footer from "../components/Footer";
+import NavbarDashboard from "../components/NavbarDashboard";
+
+function Dashboard() {
+    return (
+        <div className="LINESeed">
+            <NavbarDashboard />
+            <br />
+            <br />
+            <div className="h-screen flex items-center justify-center p-10">
+                <iframe src="https://app.powerbi.com/view?r=eyJrIjoiMTljYWM1YzUtYTAwYi00OTZhLTg0ZjYtNWFjN2FmODdkZjgzIiwidCI6IjRhNGY3YjUyLTBlMDUtNDQxNS04NDU0LTc2ODliMDBhODdiMiIsImMiOjEwfQ%3D%3D"
+                    width={1000}
+                    height={660}
+
+                />
+            </div>
+            <ScrollToTop />
+            <div className="flex flex-col">
+                <Footer />
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
